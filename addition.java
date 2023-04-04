9dc26755355d3a43f5516caf7ae6ffcc67619fc2
Atultiwari <atul@wowfinstack.com>
2023-04-05 00:15:54 +0530

    final all file commit

---
+++ addition.java
@@ -0,0 +1,8 @@
+class Addition{
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+        int c=30;
+        System.out.println("sum of two number is:-"+c);
+    }
+}
